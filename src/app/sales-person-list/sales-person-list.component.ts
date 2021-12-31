327becc1ac907de1d95c4e881245d548e25fd915
denisdimarco <denisdimarcoleda@gmail.com>
2021-12-31 05:09:47 -0300

    added boostrap, dummy data, table

--- src/app/sales-person-list/sales-person-list.component.ts
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {SalesPerson} from "./sales-person";
 
 @Component({
   selector: 'app-sales-person-list',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sales-person-list.component.css']
 })
 export class SalesPersonListComponent implements OnInit {
+
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson("John", "Doe", "johndoe@email.com", 20000),
+    new SalesPerson("Clare", "Doe", "claredoe@email.com", 30000),
+    new SalesPerson("Pepin", "Doe", "pepindoe@email.com", 40000),
+    new SalesPerson("Swan", "Doe", "swanoe@email.com", 50000)
+  ];
 
   constructor() { }
 
